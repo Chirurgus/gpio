8660af8fda4c28428bbf1fd67a14225d6b5543fd
Chirurgus <sasha.sashaxl@gmail.com>
2016-03-02 22:57:28 +0100

    Added wrk done one 02.03.16

--- source/gpio_aux.h
+++ source/gpio_aux.h
@@ -3,6 +3,6 @@
 
 #include <string>
 
-int parse_args(int argc, char** argv, const std::string &gpio_num, bool as_input);
+int parse_args(int argc, char** argv, std::string &gpio_num, bool &as_input);
 
 #endif
