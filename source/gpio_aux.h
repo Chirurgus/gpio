55feec3048894f956312e87fe919631a820dab6f
Chirurgus <sasha.sashaxl@gmail.com>
2016-03-04 23:16:41 +0100

    Work.

--- source/gpio_aux.h
+++ source/gpio_aux.h
@@ -4,5 +4,20 @@
 #include <string>
 
 int parse_args(int argc, char** argv, std::string &gpio_num, bool &as_input);
+int get_direction(const char* dir, bool &is_input);
 
+
+const char* help_string= "\t-i or --input\n\t-o or --output\n\t-h -l or --high --low\n\t-c or --check\n\t-s or --status\n\t-u or --unexport\n\t-e or --export\n";
+/*
+	gpio by Alexander
+	Syntax:gpio [ARGUMENTS] [GPIO_NUM]
+		-i or --input
+		-o or --output   Set direction to out
+		-c or --check    Get gpio value
+		-s or --set
+		-l or --status (Why -m? Cuz i'm the author)
+		-u or --unexport (delete or disactivate)
+		-e or --export 
+		-h or --help
+*/
 #endif
